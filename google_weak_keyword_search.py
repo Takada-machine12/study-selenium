8d4a926e9288ce0c0b181af15da8e6b3797ce446
TKD <takada311@gmail.com>
2026-08-11 20:42:36 +0900

    自動検索実装

--- google_weak_keyword_search.py
+++ google_weak_keyword_search.py
@@ -4,105 +4,134 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # seleniumでブラウザの検索テキストボックスの属性を取得するためにimport
 from selenium.webdriver.common.by import By
+# Google Chromeのドライバーを管理・起動するためにimport
+from selenium.webdriver.chrome.service import Service as ChromeService
 # seleniumでヘッドレスモード(裏で動作させるモード)を指定するためにimport
 from selenium.webdriver.chrome.options import Options
 # 待ち時間を指定するためにtime(プログラムを一時停止したりするのに使用)をimport
 import time
 # 正規表現にマッチする文字列を探すためにreをimport
 import re
+# エラー内容の詳細を表示するためにimport
+import traceback
 
 # Googleのトップページ
 URL = "https://www.google.com"
-'''
-メインの処理
-Googleの検索エンジンでキーワードを検索
-指定されたドメインが検索結果の１ページ目に含まれていないキーワードをテキストファイルに出力
-'''
 
-# '検索キーワードリスト.txt'ファイルを読み込み、リストにする
-with open('転職キーワードリスト.txt') as f:
-    keywords = [s.rstrip() for s in f.readlines()] # 1行ずつ取り出して、改行除去してsに入れた後、keywordsに入れる
-print(keywords)
-# 'ドメインリスト.txt'ファイルを読み込み、リストにする
-with open('ドメインリスト.txt') as f:
-    domains = [s.rstrip() for s in f.readlines()]
-print(domains)
-# １行ずつ読み込んで改行コードを削除してリストにする
+def main():
+    '''
+    メインの処理
+    Googleの検索エンジンでキーワードを検索
+    指定されたドメインが検索結果の１ページ目に含まれていないキーワードをテキストファイルに出力
+    '''
 
-# seleniumで自動操作するブラウザはGoogleChrome
-# Optionsオブジェクトを作成
-options = Options()
-# ヘッドレスモードを有効にする(ブラウザが立ち上がった時に見えない形で裏で動作させる)
-# options.add_argument('--headless')
+    # '検索キーワードリスト.txt'ファイルを読み込み、リストにする
+    with open('転職キーワードリスト.txt') as f:
+        keywords = [s.rstrip() for s in f.readlines()] # 1行ずつ取り出して、改行除去してsに入れた後、keywordsに入れる
 
-# ChromeのWebDriverオブジェクトを作成
+    # 'ドメインリスト.txt'ファイルを読み込み、リストにする
+    with open('ドメインリスト.txt') as f:
+        domains = [s.rstrip() for s in f.readlines()] # １行ずつ読み込んで改行コードを削除してリストにする
 
-# Chormeブラウザを起動させて、Pythonで操作できるようにする
-driver = webdriver.Chrome(options=options)
+    # seleniumで自動操作するブラウザはGoogleChrome
+    # Optionsオブジェクトを作成
+    options = Options()
+    # ヘッドレスモードを有効にする(ブラウザが立ち上がった時に見えない形で裏で動作させる)
+    # options.add_argument('--headless')
 
-# Googleのトップページを開く
-driver.get(URL)
-# 2秒待機（読み込みのため）
+    # ChromeのWebDriverオブジェクトを作成
+    # Chormeブラウザを起動させて、Pythonで操作できるようにする
+    service = ChromeService()
+    driver = webdriver.Chrome(service=service, options=options)
 
-# 検索キーワードを１つずつ取り出す
-# search関数実行
-# get_url関数を実行し、戻り値をurlsに代入
-# domain_checked関数を実行し、戻り値をok_keywordlistに代入
+    # Googleのトップページを開く
+    driver.get(URL)
+    # 2秒待機（読み込みのため）
+    time.sleep(2)
 
-# '結果.txt'という名前を付けて、ドメインチェックしたキーワードをファイルに書き込む
+    # 指定ドメインに含まれていないならキーワードをok_keywordlistに追加
+    ok_keywordlist = []
 
-# ドメインチェック済みのキーワードを１行ずつ保存
+    # 検索キーワードを1つずつ取り出す
+    for keyword in keywords:
+        # search関数実行
+        search(keyword, driver)
+        # get_url関数を実行し、戻り値をurlsに代入
+        urls = get_url(driver)
+        # domain_checked関数を実行し、戻り値をweak_ok_keywordlistに代入
+        weak_ok_keywordlist = domain_checked(urls, domains, ok_keywordlist, keyword)
 
-# ブラウザーを閉じる
+    # '結果.txt'という名前を付けて、ドメインチェックしたキーワードをファイルに書き込む
+    # ドメインチェック済みのキーワードを１行ずつ保存
+    with open('結果.txt', 'w') as f:
+        f.write('\n'.join(weak_ok_keywordlist))
 
-'''
-検索テキストボックスに検索キーワードを入力し、検索する
-'''
+    # ブラウザを閉じる
+    driver.quit()
 
-# 検索テキストボックスの要素をname属性から取得
-input_element = driver.find_element(By.NAME,"q")
-# 検索テキストボックスに入力されている文字列を消去
-input_element.clear()
-# 検索テキストボックスにキーワードを入力
-input_element.send_keys("転職")
-# Enterキーを送信
-input_element.send_keys(Keys.RETURN)
-# 2秒待機
-time.sleep(2)
-'''
-検索結果ページの1位から10位までのURLを取得
-'''
+def search(keyword, driver):
+    '''
+    検索テキストボックスに検索キーワードを入力し、検索する
+    '''
 
-# 各ページのURLを入れるために空のリストを用意
-urls = []
-# a要素（各ページの1位から10位までのURL）取得
-objects = driver.find_elements(By.TAG_NAME, "a")
-# rso > div:nth-child(7) > div > div > div > div.kb0PBd.A9Y9g.jGGQ5e > div > div > span > a
+    # 検索テキストボックスの要素をname属性から取得
+    input_element = driver.find_element(By.NAME,"q")
+    # 検索テキストボックスに入力されている文字列を消去
+    input_element.clear()
+    # 検索テキストボックスにキーワードを入力
+    input_element.send_keys(keyword)
+    # Enterキーを送信
+    input_element.send_keys(Keys.RETURN)
+    # 2秒待機
+    time.sleep(2)
+    # タイトルにキーワードが含まれていることを確認
 
-# a要素の取得有無で処理を分ける
-if objects:
-    for object in objects:
-        urls.append(object.get_attribute("href"))
-else:
-    print("URLを取得できませんでした。")
-# 各ページのURLを戻り値に指定
+def get_url(driver):
+    '''
+    検索結果ページの1位から10位までのURLを取得
+    '''
 
-'''
-URLリストからドメインを取得し、指定ドメインに含まれているかチェック
-'''
-# URLリストから各ページのURLを１つずつ取り出す
+    # 各ページのURLを入れるために空のリストを用意
+    urls = []
+    # a要素（各ページの1位から10位までのURL）取得
+    objects = driver.find_elements(By.CSS_SELECTOR, "#search a")
+    #tsuid_n-p6aqH2LcDX1e8P6ZOF0AE_41 > div > div:nth-child(2) > div:nth-child(3) > div > div > div > div > div > div.v5yQqb > a
 
-# '//〇〇/'に一致する箇所（ドメイン）を抜き出す
-# '//〇〇/'の'〇〇'に一致する箇所を抜き出し、domainに代入
-# ドメインに'www.'が含まれているかチェック
-# 含まれているなら'www.'を除去
-# 各ページのドメインが指定ドメインに含まれているかチェック
-# 含まれているなら警告を出す
-# １つでも含まれているなら他はチェックする必要がないので関数を終了
-# 指定ドメインに含まれていないならキーワードをok_keywordlistに追加
-# ドメインチェック済みのキーワードを戻り値に指定
+    # a要素の取得有無で処理を分ける
+    if objects:
+        for object in objects:
+            urls.append(object.get_attribute("href"))
+    else:
+        print("URLを取得できませんでした。")
+    # 各ページのURLを戻り値に指定
+    return urls
+
+
+def domain_checked(urls, domains, ok_keywordlist, keyword):
+    '''
+    URLリストからドメインを取得し、指定ドメインに含まれているかチェック
+    '''
+    # 取得・保存したURLリストから各ページのURLを１つずつ取り出す
+    for url in urls:
+        m = re.search(r'//(.*?)/', url) # '//〇〇/'に一致する箇所（ドメイン）を抜き出す
+        domain = m.group(1)            # '//〇〇/'の'〇〇'に一致する箇所を抜き出し、domainに代入
+        
+        if 'www.' in domain:            # ドメインに'www.'が含まれているかチェック
+            domain = domain[4:]         # 含まれているなら'www.'を除去
+        if domain in domains:           # 各ページのドメインが指定ドメイン(ドメインリスト.txt)に含まれているかチェック
+            # 整形したキーワードが大手キーワードに含まれているなら警告を出す
+            print(f"キーワード「{keyword}」の検索結果には大手ドメインがありましたので除外します。")
+            break                       # １つでも含まれているなら他はチェックする必要がないので関数を終了
+    else:                               # 大手キーワードではないキーワードであれば、リストに保存
+        ok_keywordlist.append(keyword)
+    # ドメインチェック済みのキーワードを戻り値に指定
+    return ok_keywordlist
 
 # main関数を実行
-
+try:
+    if __name__ == '__main__':
+        main()
+except Exception as e:
+    print(f"エラー内容：{e}")
+    traceback.print_exc()
 input("Enterを押すまでChromeを開いたままにします...")
-driver.quit()
